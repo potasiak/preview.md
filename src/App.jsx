1ab307fe2f47a981a952de31f57f583fe251b25d
Sebastian Potasiak <sebastian@potasiak.dev>
2023-12-19 19:25:09 +0100

    Add CodeMirror

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,35 @@ import { defaultContent, rehypeSanitizeOptions } from './config.js';
 import 'github-markdown-css/github-markdown-light.css';
 
 import remarkGfm from 'remark-gfm';
-
 import rehypeRaw from 'rehype-raw';
 import rehypeSanitize from 'rehype-sanitize';
 import remarkGemoji from 'remark-gemoji';
 
-
-const defaultState = {
-  fileName: 'preview.md',
-  content: defaultContent,
-};
+import CodeMirror, { EditorView } from '@uiw/react-codemirror';
+import { languages } from '@codemirror/language-data';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { githubLight } from '@uiw/codemirror-theme-github';
 
 export const App = () => {
-  const [ state, setState ] = React.useState(defaultState);
-  const handleChangeContent = (e) => setState(prevState => ({
-    ...prevState,
-    content: e.target.value,
-  }));
+  const [ content, setContent ] = React.useState(defaultContent);
 
   return (
     <React.Fragment>
-      <header className="flex bg-gray-200 px-4 py-2 justify-center">
+      <header className="flex bg-gray-200 px-4 py-2">
         <Link className="font-medium hover:underline" to="/">
           Markdown Preview
         </Link>
       </header>
       <main className="flex flex-1 overflow-hidden">
-        <div className="box-border flex flex-col p-4 basis-1/2 shrink-0 grow-0">
-          <textarea
-            className="flex-1 font-mono text-sm resize-none rounded-md border py-1.5 px-2 text-gray-900 placeholder:text-gray-400"
-            placeholder="Enter your Markdown content here..."
-            value={state.content}
-            onChange={handleChangeContent}
+        <div className="box-border flex flex-col basis-1/2 shrink-0 grow-0 overflow-y-scroll">
+          <CodeMirror
+            theme={githubLight}
+            extensions={[
+              markdown({ base: markdownLanguage, codeLanguages: languages }),
+              EditorView.lineWrapping
+            ]}
+            value={content}
+            onChange={setContent}
           />
         </div>
         <div className="box-border flex flex-col p-4 basis-1/2 shrink-0 grow-0 overflow-y-scroll">
@@ -50,7 +47,7 @@ export const App = () => {
               rehypeSanitize(rehypeSanitizeOptions)
             ]}
           >
-            {state.content}
+            {content}
           </Markdown>
         </div>
       </main>
