6a07a40c1ca42034adcfac210c859448ac5dbaa2
Sebastian Potasiak <sebastian@potasiak.dev>
2023-12-20 13:05:40 +0100

    Add syntax highlighting in preview

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,66 @@ import rehypeRaw from 'rehype-raw';
 import rehypeSanitize from 'rehype-sanitize';
 import remarkGemoji from 'remark-gemoji';
 
+import rangeParser from 'parse-numeric-range';
+import { Prism } from 'react-syntax-highlighter';
+import './prism-github.scss';
+
 import CodeMirror, { EditorView } from '@uiw/react-codemirror';
 import { languages } from '@codemirror/language-data';
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 import { githubLight } from '@uiw/codemirror-theme-github';
+
 import { ScrollSync, ScrollSyncPane } from 'react-scroll-sync/src';
+
 
 export const App = () => {
   const [ content, setContent ] = React.useState(defaultContent);
   const [ scrollSync, setScrollSync ] = React.useState(true);
 
   const handleChangeScrollSync = (e) => setScrollSync(e.target.checked);
+
+  const markdownComponents = {
+    code({ node, inline, className, ...props }) {
+      const hasLang = /language-(\w+)/.exec(className || '');
+      const hasMeta = node?.data?.meta;
+
+      const applyHighlights = (applyHighlights) => {
+        if (hasMeta) {
+          const RE = /{([\d,-]+)}/;
+          const metadata = node.data.meta?.replace(/\s/g, '');
+          const strlineNumbers = RE?.test(metadata)
+            ? RE?.exec(metadata)[1]
+            : '0';
+          const highlight = rangeParser(strlineNumbers);
+          const data = highlight.includes(applyHighlights)
+            ? 'highlight'
+            : null;
+          return { data };
+        } else {
+          return {};
+        }
+      };
+
+      const PreTag = ({ children }) => <React.Fragment>{children}</React.Fragment>
+      const CodeTag = ({ children }) => <code className={className}>{children}</code>;
+
+      return hasLang ? (
+        <Prism
+          language={hasLang[1]}
+          PreTag={PreTag}
+          CodeTag={CodeTag}
+          showLineNumbers={false}
+          wrapLines={hasMeta}
+          useInlineStyles={false}
+          lineProps={applyHighlights}
+        >
+          {props.children}
+        </Prism>
+      ) : (
+        <CodeTag>{props.children}</CodeTag>
+      );
+    },
+  };
 
   return (
     <React.Fragment>
@@ -53,6 +102,7 @@ export const App = () => {
             <div className="box-border flex flex-col p-4 basis-1/2 shrink-0 grow-0 overflow-y-scroll">
               <Markdown
                 className="markdown-body"
+                components={markdownComponents}
                 remarkPlugins={[ remarkGfm, remarkGemoji ]}
                 rehypePlugins={[
                   rehypeRaw,
