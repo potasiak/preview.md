9a21f1cc00caa53f58bab31ebaa7303d38eefb5e
Sebastian Potasiak <sebastian@potasiak.dev>
2023-12-20 11:39:47 +0100

    Add scroll sync

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,13 @@ import CodeMirror, { EditorView } from '@uiw/react-codemirror';
 import { languages } from '@codemirror/language-data';
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 import { githubLight } from '@uiw/codemirror-theme-github';
+import { ScrollSync, ScrollSyncPane } from 'react-scroll-sync/src';
 
 export const App = () => {
   const [ content, setContent ] = React.useState(defaultContent);
+  const [ scrollSync, setScrollSync ] = React.useState(true);
+
+  const handleChangeScrollSync = (e) => setScrollSync(e.target.checked);
 
   return (
     <React.Fragment>
@@ -25,32 +29,42 @@ export const App = () => {
         <Link className="font-medium hover:underline" to="/">
           Markdown Preview
         </Link>
+        <div className="flex gap-2 ms-auto">
+          <input type="checkbox" id="id_scrollSync" checked={scrollSync} onChange={handleChangeScrollSync} />
+          <label htmlFor="id_scrollSync">Scroll sync</label>
+        </div>
       </header>
-      <main className="flex flex-1 overflow-hidden">
-        <div className="box-border flex flex-col basis-1/2 shrink-0 grow-0 overflow-y-scroll">
-          <CodeMirror
-            theme={githubLight}
-            extensions={[
-              markdown({ base: markdownLanguage, codeLanguages: languages }),
-              EditorView.lineWrapping
-            ]}
-            value={content}
-            onChange={setContent}
-          />
-        </div>
-        <div className="box-border flex flex-col p-4 basis-1/2 shrink-0 grow-0 overflow-y-scroll">
-          <Markdown
-            className="markdown-body"
-            remarkPlugins={[ remarkGfm, remarkGemoji ]}
-            rehypePlugins={[
-              rehypeRaw,
-              rehypeSanitize(rehypeSanitizeOptions)
-            ]}
-          >
-            {content}
-          </Markdown>
-        </div>
-      </main>
+      <ScrollSync enabled={scrollSync}>
+        <main className="flex flex-1 overflow-hidden">
+          <ScrollSyncPane>
+            <div className="box-border flex flex-col basis-1/2 shrink-0 grow-0 overflow-y-scroll">
+              <CodeMirror
+                theme={githubLight}
+                extensions={[
+                  markdown({ base: markdownLanguage, codeLanguages: languages }),
+                  EditorView.lineWrapping
+                ]}
+                value={content}
+                onChange={setContent}
+              />
+            </div>
+          </ScrollSyncPane>
+          <ScrollSyncPane>
+            <div className="box-border flex flex-col p-4 basis-1/2 shrink-0 grow-0 overflow-y-scroll">
+              <Markdown
+                className="markdown-body"
+                remarkPlugins={[ remarkGfm, remarkGemoji ]}
+                rehypePlugins={[
+                  rehypeRaw,
+                  rehypeSanitize(rehypeSanitizeOptions)
+                ]}
+              >
+                {content}
+              </Markdown>
+            </div>
+          </ScrollSyncPane>
+        </main>
+      </ScrollSync>
     </React.Fragment>
   );
 };
